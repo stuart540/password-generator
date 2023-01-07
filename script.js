090e652bb5df3c5fd1f406e921ca6bcfb5222ee9
stuart540 <114927873+stuart540@users.noreply.github.com>
2023-01-07 16:33:24 +0000

    added pseudoCode to script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// User story: I want to ba able to generate a random password with the flexibility to select variables of length and complexity from an easy to use interface.
+
+// As a developer I want to create a password generator with easy to understand code, preferable modular to improve readability and allow for modifications.
+
+// * Parameters of the password length are between 10 and 64 characters - Check for this at the end?
+// * Password must have options for selecting; Special characters, Numeric characters, Uppercase and Lowercase characters
+
+// First a prompt() should be displayed on the page to request the desired password length - between 10 and 64 characters.
+// if input !== 10 - 64 then alert("Please see resource, how to count above 10 with socks on") and loop until true. This should also catch any other characters like symbols and letters.
+
+// Next a confirm() should offer to include a type of character, if true then add to an array, else move to next character.
+// After the 4 character options have been offered and decided, a check should run to make sure at least one type of character was selected.
+// i.e. if var1 || var2 || var 3 || var 4 === true then proceed, else alert("you must select at least one character type")
+
+// Once user has selected the characters, the family of characters could be moved into a new array called passwordGen, a random selection can then run through all the options until password length === user selected length.
+// after the password has been generated to the correct length, it should then be checked it has all the requested variables in: if true, then output to user, else run generator again.
+
+
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
