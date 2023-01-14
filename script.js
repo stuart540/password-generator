4672e9b23ebd92fa43b80bffb519688a02f0095e
stuart540 <114927873+stuart540@users.noreply.github.com>
2023-01-14 12:12:17 +0000

    wrapped code inside another function to allow a variable to be reused without affecting when the prompt is called

--- script.js
+++ script.js
@@ -107,140 +107,149 @@ var upperCasedCharacters = [
 ];
 
 var generatorArr = []
+var randomArr = []
+var passwordInt = 0
 var values = { 
   characterSet: [specialCharacters, numericCharacters, upperCasedCharacters, lowerCasedCharacters],
   promptName: ["special", "numeric", "lower", "upper"]
-
-}
-
-console.log(values.characterSet.length - 1); 
-
-// Function to prompt for password input
-function getPasswordLength() {
-  // to clear array with each click of button
-  generatorArr.length = 0;
-  var passwordLength = parseInt(prompt("Please select the required length of the password", "10 - 64"));
-  if (passwordLength >= 10 && passwordLength <= 64) {
-    console.log(passwordLength);
-    getPasswordOptions()
-  }
-  else
-  alert("Please google 'how to count above 10 with socks on', then try again with a number between 10 and 64 inclusive.");
-  getPasswordLength()
-}
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-  for (var i = 0; i < values.characterSet.length; i++) {
-    var text = "Does your password require " + values.promptName[i] + " characters?\nOk for yes, Cancel for no.";
-    if (confirm(text) == true) {
-    generatorArr = generatorArr.concat(values.characterSet[i]);
-    
-    }
-    
-    if ([i] == values.characterSet.length - 1 && generatorArr.length !== 0) {
-      alert("Making password... this may take a while");
-      alert("Seriously, you should probably make a coffee and come back in 5");
-
-      // randomCharacter(generatorArr)
-    }
-
-    else if ([i] == values.characterSet.length - 1 && generatorArr.length === 0) {
-      alert("you must select at least one type of character");
-      getPasswordOptions()
-    }
-
-      console.log(generatorArr);
-      
-      // else count, if reaches 4 then alert?
-    
-  }
-
-}
-
-// specialCharactersFunction();
-// numericCharactersFunction();
-// lowerCasedCharactersFunction();
-// upperCasedCharactersFunction();
-
-
-// function specialCharactersFunction() {
-  //   let text = "Does your password require special characters?\nOk for yes, Cancel for no.";
-//   if (confirm(text) == true) {
-//      // move selected to arr
-//     generatorArr = generatorArr.concat(specialCharacters);
-//     console.log(generatorArr);
-//   }
-
-// }
-
-
-// function numericCharactersFunction() {
-//   let text = "Does your password require numeric characters?\nOk for yes, Cancel for no.";
-//   if (confirm(text) == true) {
-//     // move selected to arr
-//     generatorArr = generatorArr.concat(numericCharacters);
-//     console.log(generatorArr);
-//   }
-// }
-
-// function lowerCasedCharactersFunction() {
-//   let text = "Does your password require lower cased characters?\nOk for yes, Cancel for no.";
-//   if (confirm(text) == true) {
-//     // move selected to arr
-//     generatorArr = generatorArr.concat(lowerCasedCharacters);
-//     console.log(generatorArr);
-//   }
-// }
-
-// function upperCasedCharactersFunction() {
-//   let text = "Does your password require upper cased characters?\nOk for yes, Cancel for no.";
-//   if (confirm(text) == true) {
-//     // move selected to arr
-//     generatorArr = generatorArr.concat(upperCasedCharacters);
-//     console.log(generatorArr);
-//   }
-//   else alert ("you must select at least one type of character")
-// }
-
-//• Need to have a way to alert character select if none are selected
-
-// if (specialCharactersFunction() == false) {
-//   alert ("you must select at least one type of character")
   
-// }
-
-  // else alert ("you must select at least one type of character")
-
-
-
-
-// Function for getting a random element from an array
-// function getRandom(arr) {
-//   for (let j = 0; j < passwordLength.length; j++) {
-//     passwordFinal = generatorArr.push(generatorArr[j]);
-//     console.log(passwordFinal);
-    
-//   }
-//   var x = Math.floor((Math.random() * passwordLength.length) + 1);
-
-// }
-
-// getRandom()
-
-
-//   function randomCharacter(generatorArr) {
-    
-//   return generatorArr[Math.floor(Math.random()*generatorArr.length)];
-      
-//   } {
-//   console.log(randomCharacter());
-// }
+}
+// arr[Math.floor(Math.random() * arr.length)];
 
 // Function to generate password with user input
 function generatePassword() {
+  var passwordLength = parseInt(prompt("Please select the required length of the password", "10 - 64"));
+  getPasswordLength()
+  // var randomCharacter = function(arr) {
+  //   for (let j = 0; j < passwordLength.length; j++) {
+  //     var randomArr = randomArr[j];
+      
+  //   }
+    
+  // } 
+  
+  // Function to prompt for password input
+  function getPasswordLength() {
+    // to clear array with each click of button
+    generatorArr.length = 0;
+    if (passwordLength >= 10 && passwordLength <= 64) {
+      
+      getPasswordOptions()
+    }
+    
+    else
+    alert("Please google 'how to count above 10 with socks on', then try again with a number between 10 and 64 inclusive.");
+    generatePassword()
+    
+  }
+  
+  console.log(passwordLength);
+  // Function to prompt user for password options
+  function getPasswordOptions() {
+    for (var i = 0; i < values.characterSet.length; i++) {
+      var text = "Does your password require " + values.promptName[i] + " characters?\nOk for yes, Cancel for no.";
+      if (confirm(text) == true) {
+      generatorArr = generatorArr.concat(values.characterSet[i]);
+      
+      }
+
+      if ([i] == values.characterSet.length - 1 && generatorArr.length !== 0) {
+        alert("Making password... this may take a while");
+        alert("Seriously, you should probably make a coffee and come back in 5");
+        
+        randomCharacter(generatorArr) //?maybe char.length here?
+        console.log(generatorArr);
+        console.log(randomCharacter);
+        
+      }
+
+      else if ([i] == values.characterSet.length - 1 && generatorArr.length === 0) {
+        alert("you must select at least one type of character");
+        getPasswordOptions()
+      }
+
+        console.log(generatorArr);
+
+      
+    }
+  }
+
+  //? Maybe wrap everything inside a function which is called on click?
+
+  // specialCharactersFunction();
+  // numericCharactersFunction();
+  // lowerCasedCharactersFunction();
+  // upperCasedCharactersFunction();
+
+
+  // function specialCharactersFunction() {
+    //   let text = "Does your password require special characters?\nOk for yes, Cancel for no.";
+  //   if (confirm(text) == true) {
+  //      // move selected to arr
+  //     generatorArr = generatorArr.concat(specialCharacters);
+  //     console.log(generatorArr);
+  //   }
+
+  // }
+
+
+  // function numericCharactersFunction() {
+  //   let text = "Does your password require numeric characters?\nOk for yes, Cancel for no.";
+  //   if (confirm(text) == true) {
+  //     // move selected to arr
+  //     generatorArr = generatorArr.concat(numericCharacters);
+  //     console.log(generatorArr);
+  //   }
+  // }
+
+  // function lowerCasedCharactersFunction() {
+  //   let text = "Does your password require lower cased characters?\nOk for yes, Cancel for no.";
+  //   if (confirm(text) == true) {
+  //     // move selected to arr
+  //     generatorArr = generatorArr.concat(lowerCasedCharacters);
+  //     console.log(generatorArr);
+  //   }
+  // }
+
+  // function upperCasedCharactersFunction() {
+  //   let text = "Does your password require upper cased characters?\nOk for yes, Cancel for no.";
+  //   if (confirm(text) == true) {
+  //     // move selected to arr
+  //     generatorArr = generatorArr.concat(upperCasedCharacters);
+  //     console.log(generatorArr);
+  //   }
+  //   else alert ("you must select at least one type of character")
+  // }
+
+  //• Need to have a way to alert character select if none are selected
+
+  // if (specialCharactersFunction() == false) {
+  //   alert ("you must select at least one type of character")
+    
+  // }
+
+    // else alert ("you must select at least one type of character")
+
+
+
+
+  // Function for getting a random element from an array
+  function getRandom(arr) {
+    for (let j = 0; j < passwordLength.length; j++) {
+      passwordFinal = generatorArr.push(generatorArr[j]);
+      console.log(passwordFinal);
+      
+    }
+    var x = Math.floor((Math.random() * passwordLength.length) + 1);
+
+  }
+
+  getRandom()
 
 }
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
